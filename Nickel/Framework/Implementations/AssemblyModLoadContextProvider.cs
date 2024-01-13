Shockah/Nickel

commit ceea919684066bc8c63dbd0045521badffaf7005

Load non-shared assemblies in the CustomContext, not the shared one
This fixes an issue where second-level dependencies
(e.g. `Mod -> Lib1 -> Lib2`) wouldn't get loaded, as `Lib1` was
loaded in the shared context rather than the mod-individual context.

Nickel/Framework/Implementations/AssemblyModLoadContextProvider.cs
@@ -77,11 +77,11 @@ internal sealed class AssemblyModLoadContextProvider(
 				try
 				{
 					using var assemblyStream = this.Package.PackageRoot.GetRelativeFile($"{assemblyName.Name ?? assemblyName.FullName}.dll").OpenRead();
-					return this.SharedContext.LoadFromStream(assemblyStream);
+					return this.LoadFromStream(assemblyStream);
 				}
 				catch
 				{
-					return this.SharedContext.LoadFromAssemblyName(assemblyName);
+					return this.LoadFromAssemblyName(assemblyName);
 				}
 			}
 		}

commit c2ecc48206ece02281cfc4516301066222013bf7

Correct assembly loading
Fixes an issue introduced in ceea919684066bc8c63dbd0045521badffaf7005
where falling back to a shared assembly during non-shared loading
would instead try loading non-sharedly again (causing a stack overflow)

Nickel/Framework/Implementations/AssemblyModLoadContextProvider.cs
@@ -81,7 +81,7 @@ internal sealed class AssemblyModLoadContextProvider(
 				}
 				catch
 				{
-					return this.LoadFromAssemblyName(assemblyName);
+					return this.SharedContext.LoadFromAssemblyName(assemblyName);
 				}
 			}
 		}