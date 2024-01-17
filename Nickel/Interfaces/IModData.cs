3851c6719b3d2e533eda3a607dd2828892fb009f
Michał Dolaś <me@shockah.pl>
2024-01-17 18:21:55 +0100

    Additional ModData extensions

--- Nickel/Interfaces/IModData.cs
+++ Nickel/Interfaces/IModData.cs
@@ -15,3 +15,31 @@ public interface IModData
 	void SetModData<T>(object o, string key, T data);
 	void RemoveModData(object o, string key);
 }
+
+public static class IModDataClassExt
+{
+	public static T? GetOptionalModData<T>(this IModData modData, object o, string key) where T : class
+		=> modData.TryGetModData<T>(o, key, out var data) ? data : null;
+
+	public static void SetOptionalModData<T>(this IModData modData, object o, string key, T? data) where T : class
+	{
+		if (data is { } nonNull)
+			modData.SetModData(o, key, nonNull);
+		else
+			modData.RemoveModData(o, key);
+	}
+}
+
+public static class IModDataStructExt
+{
+	public static T? GetOptionalModData<T>(this IModData modData, object o, string key) where T : struct
+		=> modData.TryGetModData<T>(o, key, out var data) ? data : null;
+
+	public static void SetOptionalModData<T>(this IModData modData, object o, string key, T? data) where T : struct
+	{
+		if (data is { } nonNull)
+			modData.SetModData(o, key, nonNull);
+		else
+			modData.RemoveModData(o, key);
+	}
+}
