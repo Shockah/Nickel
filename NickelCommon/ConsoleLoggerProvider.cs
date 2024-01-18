d64afbf9621d81ae3a7aeb3ccc48c928db22f2da
Michał Dolaś <me@shockah.pl>
2024-01-18 23:37:08 +0100

    Changed console logging to a more compatible way

--- NickelCommon/ConsoleLoggerProvider.cs
+++ NickelCommon/ConsoleLoggerProvider.cs
@@ -1,15 +1,11 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
-using System.Text;
 
 namespace Nickel;
 
 public sealed class ConsoleLoggerProvider(LogLevel level, TextWriter textWriter, bool disposeWriter) : ILoggerProvider
 {
-	private const string DefaultForegroundColor = "\x1B[39m\x1B[22m"; // reset to default foreground color
-	private const string DefaultBackgroundColor = "\x1B[49m"; // reset to the background color
-
 	public void Dispose()
 	{
 		textWriter.Flush();
@@ -38,17 +34,32 @@ public sealed class ConsoleLoggerProvider(LogLevel level, TextWriter textWriter,
 		{
 			if (!this.IsEnabled(logLevel))
 				return;
+			var logColor = GetLogLevelConsoleColors(logLevel);
 
-			textWriter.Write($"[{GetColoredLogLevelString(logLevel)}]");
-			textWriter.Write($"[{categoryName}]");
-			textWriter.Write($" {formatter(state, exception)}");
-			textWriter.WriteLine();
-		}
+			var oldBackgroundColor = Console.BackgroundColor;
+			var oldForegroundColor = Console.ForegroundColor;
+			try
+			{
+				textWriter.Write('[');
 
-		private static string GetColoredLogLevelString(LogLevel logLevel)
-		{
-			var colors = GetLogLevelConsoleColors(logLevel);
-			return ColoredMessage(GetLogLevelString(logLevel), colors.Background, colors.Foreground);
+				Console.BackgroundColor = logColor.Background ?? oldBackgroundColor;
+				Console.ForegroundColor = logColor.Foreground ?? oldForegroundColor;
+				textWriter.Write(GetLogLevelString(logLevel));
+
+				Console.BackgroundColor = oldBackgroundColor;
+				Console.ForegroundColor = oldForegroundColor;
+				textWriter.Write(']');
+
+				textWriter.Write($"[{categoryName}]");
+				textWriter.Write($" {formatter(state, exception)}");
+				textWriter.WriteLine();
+
+			}
+			finally
+			{
+				Console.BackgroundColor = oldBackgroundColor;
+				Console.ForegroundColor = oldForegroundColor;
+			}
 		}
 
 		private static string GetLogLevelString(LogLevel logLevel)
@@ -74,57 +85,6 @@ public sealed class ConsoleLoggerProvider(LogLevel level, TextWriter textWriter,
 				LogLevel.Critical => new ConsoleColors { Foreground = ConsoleColor.White, Background = ConsoleColor.DarkRed },
 				_ => new ConsoleColors()
 			};
-
-		private static string GetForegroundColorEscapeCode(ConsoleColor color)
-			=> color switch
-			{
-				ConsoleColor.Black => "\x1B[30m",
-				ConsoleColor.DarkRed => "\x1B[31m",
-				ConsoleColor.DarkGreen => "\x1B[32m",
-				ConsoleColor.DarkYellow => "\x1B[33m",
-				ConsoleColor.DarkBlue => "\x1B[34m",
-				ConsoleColor.DarkMagenta => "\x1B[35m",
-				ConsoleColor.DarkCyan => "\x1B[36m",
-				ConsoleColor.Gray => "\x1B[37m",
-				ConsoleColor.Red => "\x1B[1m\x1B[31m",
-				ConsoleColor.Green => "\x1B[1m\x1B[32m",
-				ConsoleColor.Yellow => "\x1B[1m\x1B[33m",
-				ConsoleColor.Blue => "\x1B[1m\x1B[34m",
-				ConsoleColor.Magenta => "\x1B[1m\x1B[35m",
-				ConsoleColor.Cyan => "\x1B[1m\x1B[36m",
-				ConsoleColor.White => "\x1B[1m\x1B[37m",
-				_ => DefaultForegroundColor // default foreground color
-			};
-
-		private static string GetBackgroundColorEscapeCode(ConsoleColor color)
-			=> color switch
-			{
-				ConsoleColor.Black => "\x1B[40m",
-				ConsoleColor.DarkRed => "\x1B[41m",
-				ConsoleColor.DarkGreen => "\x1B[42m",
-				ConsoleColor.DarkYellow => "\x1B[43m",
-				ConsoleColor.DarkBlue => "\x1B[44m",
-				ConsoleColor.DarkMagenta => "\x1B[45m",
-				ConsoleColor.DarkCyan => "\x1B[46m",
-				ConsoleColor.Gray => "\x1B[47m",
-				_ => DefaultBackgroundColor // Use default background color
-			};
-
-		private static string ColoredMessage(string message, ConsoleColor? background, ConsoleColor? foreground)
-		{
-			StringBuilder sb = new();
-			// Order: backgroundcolor, foregroundcolor, Message, reset foregroundcolor, reset backgroundcolor
-			if (background is not null)
-				sb.Append(GetBackgroundColorEscapeCode(background.Value));
-			if (foreground is not null)
-				sb.Append(GetForegroundColorEscapeCode(foreground.Value));
-			sb.Append(message);
-			if (foreground is not null)
-				sb.Append(DefaultForegroundColor); // reset to default foreground color
-			if (background is not null)
-				sb.Append(DefaultBackgroundColor); // reset to the background color
-			return $"{sb}";
-		}
 
 		private readonly struct ConsoleColors
 		{
