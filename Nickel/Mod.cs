a84975cd55cca4e4e5c27e6dc5d66bba1cfd26a5
Fayti1703 <fayti1703@protonmail.com>
2024-01-28 16:45:35 +0100

    Add `UsedImplicitly` attribute to `Nickel.Mod` class

--- Nickel/Mod.cs
+++ Nickel/Mod.cs
@@ -1,5 +1,8 @@
+using JetBrains.Annotations;
+
 namespace Nickel;
 
+[UsedImplicitly(ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature, ImplicitUseTargetFlags.WithInheritors)]
 public abstract class Mod
 {
 	public virtual object? GetApi(IModManifest requestingMod)
