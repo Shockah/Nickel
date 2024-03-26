e29e8bc2ece447815928d721dd62dd26e5a182d5
Michał Dolaś <me@shockah.pl>
2024-03-26 11:48:04 +0100

    Added some Nickel project XML docs

--- Nickel/Utilities/StreamExt.cs
+++ Nickel/Utilities/StreamExt.cs
@@ -2,8 +2,17 @@ using System.IO;
 
 namespace Nickel;
 
+/// <summary>
+/// Hosts extensions for working with <see cref="Stream"/>s.
+/// </summary>
 public static class StreamExt
 {
+	/// <summary>
+	/// Reads the remaining data in the stream and copies it over to a new <see cref="MemoryStream"/>.
+	/// </summary>
+	/// <param name="stream">The stream to copy.</param>
+	/// <param name="takeOwnership">Whether the original stream should be closed after reading all of its data.</param>
+	/// <returns></returns>
 	public static MemoryStream ToMemoryStream(this Stream stream, bool takeOwnership = true)
 	{
 		try
