1418e02828fe4472a772096a614a870caf5d6caa
Michał Dolaś <me@shockah.pl>
2024-09-14 09:54:24 +0200

    Incorporated Kokoro's card action rendering fixes

--- Nickel.Essentials/ModEntry.cs
+++ Nickel.Essentials/ModEntry.cs
@@ -72,6 +72,7 @@ public sealed class ModEntry : SimpleMod
 
 		var harmony = helper.Utilities.Harmony;
 		ArtifactCodexFixes.ApplyPatches(harmony);
+		CardActionRenderingFixes.ApplyPatches(harmony);
 		CardBrowseCurrentPile.ApplyPatches(harmony);
 		CardCodexFiltering.ApplyPatches(harmony);
 		CrewSelection.ApplyPatches(harmony);
