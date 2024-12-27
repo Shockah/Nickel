a72d6388e3642265b313edbdc26f9b877db47213
Michał Dolaś <me@shockah.pl>
2024-12-28 00:40:41 +0100

    Improvements

--- Nickel/Utilities/LinqExt.cs
+++ Nickel/Utilities/LinqExt.cs
@@ -16,7 +16,13 @@ public static class LinqExt
 	/// <param name="self">The enumerable.</param>
 	/// <returns>The first element or <c>null</c> if there are none.</returns>
 	public static T? FirstOrNull<T>(this IEnumerable<T> self) where T : struct
-		=> self.Select(e => new T?(e)).FirstOrDefault();
+	{
+		if (self.TryGetNonEnumeratedCount(out var count) && count > 0)
+			return self.First();
+		foreach (var element in self)
+			return element;
+		return null;
+	}
 
 	/// <summary>
 	/// Returns the first element matching a predicate or <c>null</c> if there are none.
@@ -26,7 +32,12 @@ public static class LinqExt
 	/// <param name="predicate">The predicate.</param>
 	/// <returns>The first element matching a predicate or <c>null</c> if there are none.</returns>
 	public static T? FirstOrNull<T>(this IEnumerable<T> self, Func<T, bool> predicate) where T : struct
-		=> self.Where(predicate).Select(e => new T?(e)).FirstOrDefault();
+	{
+		foreach (var element in self)
+			if (predicate(element))
+				return element;
+		return null;
+	}
 
 	/// <summary>
 	/// Returns the last element or <c>null</c> if there are none.
@@ -35,7 +46,13 @@ public static class LinqExt
 	/// <param name="self">The enumerable.</param>
 	/// <returns>The last element or <c>null</c> if there are none.</returns>
 	public static T? LastOrNull<T>(this IEnumerable<T> self) where T : struct
-		=> self.Select(e => new T?(e)).LastOrDefault();
+	{
+		if (self.TryGetNonEnumeratedCount(out var count) && count > 0)
+			return self.Last();
+		foreach (var element in self)
+			return element;
+		return null;
+	}
 
 	/// <summary>
 	/// Returns the last element matching a predicate or <c>null</c> if there are none.
@@ -45,5 +62,10 @@ public static class LinqExt
 	/// <param name="predicate">The predicate.</param>
 	/// <returns>The last element matching a predicate or <c>null</c> if there are none.</returns>
 	public static T? LastOrNull<T>(this IEnumerable<T> self, Func<T, bool> predicate) where T : struct
-		=> self.Where(predicate).Select(e => new T?(e)).LastOrDefault();
+	{
+		foreach (var element in self.Reverse())
+			if (predicate(element))
+				return element;
+		return null;
+	}
 }
--- Nickel/Utilities/SemanticVersionConverter.cs
+++ Nickel/Utilities/SemanticVersionConverter.cs
@@ -14,9 +14,9 @@ public sealed class SemanticVersionConverter : JsonConverter<SemanticVersion>
 	public override SemanticVersion ReadJson(JsonReader reader, Type objectType, SemanticVersion existingValue, bool hasExistingValue, JsonSerializer serializer)
 	{
 		if (reader.TokenType != JsonToken.String)
-			throw new ArgumentException($"Cannot parse {reader.Value} as {nameof(SemanticVersion)}.");
+			throw new ArgumentException($"Cannot parse {reader.Value} as {nameof(SemanticVersion)}");
 		var rawValue = JToken.Load(reader).Value<string>();
-		return SemanticVersionParser.TryParse(rawValue, out var version) ? version : throw new ArgumentException($"Cannot parse {reader.Value} as {nameof(SemanticVersion)}.");
+		return SemanticVersionParser.TryParse(rawValue, out var version) ? version : throw new ArgumentException($"Cannot parse {reader.Value} as {nameof(SemanticVersion)}");
 	}
 
 	/// <inheritdoc/>
