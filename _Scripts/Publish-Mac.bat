0fa64a8ea753c805b07248952559fe07e973e0d8
Michał Dolaś <me@shockah.pl>
2025-07-09 21:16:55 +0200

    Improved publish script

--- _Scripts/Publish-Mac.bat
+++ _Scripts/Publish-Mac.bat
@@ -3,10 +3,13 @@ pushd .
 
 set /p Version=<"..\Version.txt"
 
+rmdir /q /s "..\_Publish\osx-x64"
 xcopy /E "..\_Mac\Nickel.app" "..\_Publish\osx-x64\Nickel.app\"
 call Publish-Any.bat osx-x64 Nickel.app\Contents\MacOS
 
 cd "..\_Publish\osx-x64"
 tar.exe -a -cf "..\Nickel-!Version!-Mac.zip" "Nickel.app"
+%SCRIPTPATH%\zip_exec\zip_exec.exe "..\Nickel-!Version!-Mac.zip" Nickel.app/Contents/MacOS/Nickel
+%SCRIPTPATH%\zip_exec\zip_exec.exe "..\Nickel-!Version!-Mac.zip" Nickel.app/Contents/MacOS/NickelLauncher
 
 popd
