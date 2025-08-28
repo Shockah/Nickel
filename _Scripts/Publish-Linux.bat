77840199723fc8a02e9c9fed763336dc9e928332
Terria-K <teuria@proton.me>
2025-08-28 18:12:57 +0800

    Include NickelLinuxLauncher on the build script

--- _Scripts/Publish-Linux.bat
+++ _Scripts/Publish-Linux.bat
@@ -7,6 +7,10 @@ rmdir /q /s "..\_Publish\linux-x64"
 call Publish-Any.bat linux-x64 Nickel
 mkdir "..\_Publish\linux-x64\Nickel\ModLibrary"
 
+pushd "..\NickelLinuxLauncher"
+dotnet publish NickelLinuxLauncher.csproj -c Release -r linux-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false -p:PublishSingleFile=false -p:OutDir="..\_Publish\linux-x64\Nickel" --self-contained
+popd
+
 cd "..\_Publish\linux-x64"
 tar.exe -a -cf "..\Nickel-!Version!-Linux.zip" "Nickel"
 
